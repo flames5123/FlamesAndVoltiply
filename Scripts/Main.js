7a31c9a0aa8aeea94f3c803424776da9df849037
flames5123 <flames5123@gmail.com>
2018-07-18 11:20:05 -0500

    Update Main.js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,6 +1,19 @@
 $(window).on("load", pageLoad);
-
+String.prototype.hashCode = function () {
+    var hash = 0;
+    if (this.length == 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        char = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
 function pageLoad() {
-  console.log("testing");
-  
+    console.log('PageLoad');
+    $('#btnPasswordSubmit').click(function() {
+        if ($(this).val().toLowerCase().hashCode() = 102866733){
+            console.log('lewd view activated');
+        }
+    });
 }
