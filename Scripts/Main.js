a8d0cd6323ff1112dd4ca0c98c9d523af38e954b
flames5123 <flames5123@gmail.com>
2019-06-06 12:00:21 -0500

    Update Main.js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,2 +1,10 @@
 document.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js""><\/script>');
-console.log($('#header'))
+function defer() {
+    if (window.$) {
+      
+        console.log($('#header'));
+    } else {
+      console.log('hey');
+        setTimeout(defer(), 50);
+    }
+}
