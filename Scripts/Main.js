34b3c604a8633224dacb86869ac4abe694ca55e4
flames5123 <flames5123@gmail.com>
2019-06-06 11:54:57 -0500

    Update Main.js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,24 +1,2 @@
-$(window).on("load", pageLoad);
-
-String.prototype.hashCode = function () {
-    var hash = 0;
-    if (this.length == 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        char = this.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash;
-    }
-    return hash;
-}
-
-function pageLoad() {
-    console.log('PageLoad');
-    $('#btnPasswordSubmit').click(function() {
-        if ($('#txtPassword').val().toLowerCase().hashCode() == 102866733) {
-            console.log('lewd view activated');
-        } else {
-            console.log('wrong');
-        }
-        return false;
-    });
-}
+document.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js""><\/script>');
+console.log($('#header'))
